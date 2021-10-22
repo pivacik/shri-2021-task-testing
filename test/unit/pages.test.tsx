cce7c170176d2073cfd238a7164d5cb9918a299b
Dmitrii Pozdniakov <piv4cik@gmail.com>
2021-10-22 23:11:24 +0300

    test Cart page wether it has table or not

--- test/unit/pages.test.tsx
+++ test/unit/pages.test.tsx
@@ -4,7 +4,7 @@
 import "@testing-library/jest-dom";
 import React from "react";
 import { it, expect } from "@jest/globals";
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import { Router } from "react-router";
 import { createMemoryHistory } from "history";
 import { ExampleApi, CartApi } from "../../src/client/api";
@@ -12,6 +12,8 @@ import { Provider } from "react-redux";
 
 import { Application } from "../../src/client/Application";
 import { initStore } from "../../src/client/store";
+import { Cart } from "../../src/client/pages/Cart";
+import { CartItem } from "../../src/common/types";
 
 describe("Отображение страницы /home", () => {
   it('по адресу / должна открываться страница "Home" с приветствем', () => {
@@ -159,5 +161,64 @@ describe("Отображение страницы /cart", () => {
       getByRole("heading", { name: /shopping cart/i }).textContent
     ).toEqual("Shopping cart");
     // screen.logTestingPlaygroundURL();
+  });
+
+  it("если корзина пустая должна быть ссылка на Catalog", () => {
+    const history = createMemoryHistory({
+      initialEntries: ["/cart"],
+      initialIndex: 0,
+    });
+    const basename = "/hw/store";
+    const api = new ExampleApi(basename);
+    const cart = new CartApi();
+    cart.setState({});
+    const store = initStore(api, cart);
+
+    const application = (
+      <Router history={history}>
+        <Provider store={store}>
+          <Cart />
+        </Provider>
+      </Router>
+    );
+
+    const { getByRole } = render(application);
+    const catalogLink = getByRole("link", { name: /catalog/i });
+    expect(catalogLink.textContent).toEqual("catalog");
+  });
+
+  it("если в корзине есть товары должна быть таблица с этим товаром", () => {
+    const history = createMemoryHistory({
+      initialEntries: ["/cart"],
+      initialIndex: 0,
+    });
+    const basename = "/hw/store";
+    const api = new ExampleApi(basename);
+    const product: CartItem = {
+      name: "Product",
+      price: 222,
+      count: 1,
+    };
+    const cart = new CartApi();
+    cart.setState({ 1: product });
+    const store = initStore(api, cart);
+
+    const application = (
+      <Router history={history}>
+        <Provider store={store}>
+          <Cart />
+        </Provider>
+      </Router>
+    );
+
+    const { getByTestId } = render(application);
+
+    const tableRow = getByTestId(1);
+    const productInfoHTML = within(tableRow).getAllByRole("cell");
+    const productInfo = productInfoHTML.map((item) => item.textContent);
+
+    expect(productInfo).toContain("Product");
+    expect(productInfo).toContain("$222");
+    expect(productInfo).toContain("1");
   });
 });
