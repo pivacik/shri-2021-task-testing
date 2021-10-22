99f7fbba704c4ff97abb64c31b01c95e9f494a09
Dmitrii Pozdniakov <piv4cik@gmail.com>
2021-10-22 10:30:30 +0300

    test ProductDetails component

--- test/unit/Components.test.tsx
+++ test/unit/Components.test.tsx
@@ -71,3 +71,158 @@ describe("Отображение компонента CartBage", () => {
 `);
   });
 });
+
+describe("Компонент ProductDetails", () => {
+  it("должен отображаться", () => {
+    const basename = "/hw/store";
+    const api = new ExampleApi(basename);
+    const cart = new CartApi();
+    cart.setState({});
+    const store = initStore(api, cart);
+    const product: Product = {
+      id: 1,
+      name: "Product",
+      price: 222,
+      description: "short description",
+      material: "steel",
+      color: "white",
+    };
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <ProductDetails product={product} />
+        </Provider>
+      )
+      .toJSON();
+    expect(tree).toMatchInlineSnapshot(`
+<div
+  className="ProductDetails row"
+>
+  <div
+    className="col-12 col-sm-5 col-lg-4"
+  >
+    <img
+      className="Image"
+      src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkMAYAADkANVKH3ScAAAAASUVORK5CYII="
+    />
+  </div>
+  <div
+    className="col-12 col-sm-7 col-lg-6"
+  >
+    <h1
+      className="ProductDetails-Name"
+    >
+      Product
+    </h1>
+    <p
+      className="ProductDetails-Description"
+    >
+      short description
+    </p>
+    <p
+      className="ProductDetails-Price fs-3"
+    >
+      $
+      222
+    </p>
+    <p>
+      <button
+        className="ProductDetails-AddToCart btn btn-primary btn-lg"
+        onClick={[Function]}
+      >
+        Add to Cart
+      </button>
+    </p>
+    <dl>
+      <dt>
+        Color
+      </dt>
+      <dd
+        className="ProductDetails-Color text-capitalize"
+      >
+        white
+      </dd>
+      <dt>
+        Material
+      </dt>
+      <dd
+        className="ProductDetails-Material text-capitalize"
+      >
+        steel
+      </dd>
+    </dl>
+  </div>
+</div>
+`);
+  });
+
+  it("по нажатию на 'Add to cart' должен появляться 'Item in cart'", () => {
+    const product: Product = {
+      id: 1,
+      name: "Product",
+      price: 222,
+      description: "short description",
+      material: "steel",
+      color: "white",
+    };
+
+    const basename = "/hw/store";
+    const api = new ExampleApi(basename);
+    const cart = new CartApi();
+    cart.setState({});
+    const store = initStore(api, cart);
+    const application = (
+      <Provider store={store}>
+        <ProductDetails product={product} />
+      </Provider>
+    );
+
+    const { getByRole, getByText } = render(application);
+
+    const addToCartBtn = getByRole("button", {
+      name: /add to cart/i,
+    });
+    events.click(addToCartBtn);
+
+    expect(getByText(/item in cart/i).textContent).toEqual("Item in cart");
+  });
+
+  it("по нажатию на 'Add to cart' товар должен добавляться в корзину", () => {
+    const product: Product = {
+      id: 1,
+      name: "Product",
+      price: 222,
+      description: "short description",
+      material: "steel",
+      color: "white",
+    };
+
+    const basename = "/hw/store";
+    const api = new ExampleApi(basename);
+    const cart = new CartApi();
+    cart.setState({});
+    const store = initStore(api, cart);
+    const application = (
+      <Provider store={store}>
+        <ProductDetails product={product} />
+      </Provider>
+    );
+
+    const { getByRole } = render(application);
+
+    const addToCartBtn = getByRole("button", {
+      name: /add to cart/i,
+    });
+    events.click(addToCartBtn);
+
+    const item: CartItem = {
+      name: "Product",
+      price: 222,
+      count: 1,
+    };
+    const cartItem: CartState = {
+      1: item,
+    };
+    expect(cart.getState()).toEqual(cartItem);
+  });
+});
