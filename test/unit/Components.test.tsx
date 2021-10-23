2f3d6333190fd575e24f956d295b48765b3ac8a8
Dmitrii Pozdniakov <piv4cik@gmail.com>
2021-10-24 02:03:53 +0300

    test helmets

--- test/unit/Components.test.tsx
+++ test/unit/Components.test.tsx
@@ -9,11 +9,12 @@ import { render, screen } from "@testing-library/react";
 import events from "@testing-library/user-event";
 import { ExampleApi, CartApi } from "../../src/client/api";
 import { Provider } from "react-redux";
-import { initStore } from "../../src/client/store";
+import { initStore, productsLoaded } from "../../src/client/store";
 import { Image } from "../../src/client/components/Image";
 import { Form } from "../../src/client/components/Form";
 import { Router } from "react-router";
 import { createMemoryHistory } from "history";
+import { Helmet } from "react-helmet";
 
 import {
   CartItem,
@@ -25,7 +26,6 @@ import {
 import { CartBadge } from "../../src/client/components/CartBadge";
 import { ProductDetails } from "../../src/client/components/ProductDetails";
 import { ProductItem } from "../../src/client/components/ProductItem";
-import { delay } from "rxjs";
 import { Application } from "../../src/client/Application";
 
 describe("Отображение компонента Image", () => {
@@ -312,5 +312,99 @@ describe("Компонент Form", () => {
 
     events.click(checkoutBtn);
     expect(submitedDataSTUB).toEqual({});
+  });
+});
+
+describe("Проверка Helmet", () => {
+  const api = new ExampleApi("");
+  const cart = new CartApi();
+  cart.setState({});
+  const store = initStore(api, cart);
+
+  it("title должен быть Shopping cart", () => {
+    const history = createMemoryHistory({
+      initialEntries: ["/cart"],
+      initialIndex: 0,
+    });
+    const application = (
+      <Router history={history}>
+        <Provider store={store}>
+          <Application />
+        </Provider>
+      </Router>
+    );
+
+    render(application);
+    const helmet = Helmet.peek();
+    expect(helmet.title).toEqual("Shopping cart — Example store");
+  });
+
+  it("title должен быть Catalog", () => {
+    const history = createMemoryHistory({
+      initialEntries: ["/catalog"],
+      initialIndex: 0,
+    });
+    const application = (
+      <Router history={history}>
+        <Provider store={store}>
+          <Application />
+        </Provider>
+      </Router>
+    );
+
+    render(application);
+    const helmet = Helmet.peek();
+    expect(helmet.title).toEqual("Catalog — Example store");
+  });
+  it("title должен быть Welcome", () => {
+    const history = createMemoryHistory({
+      initialEntries: ["/"],
+      initialIndex: 0,
+    });
+    const application = (
+      <Router history={history}>
+        <Provider store={store}>
+          <Application />
+        </Provider>
+      </Router>
+    );
+
+    render(application);
+    const helmet = Helmet.peek();
+    expect(helmet.title).toEqual("Welcome — Example store");
+  });
+  it("title должен быть Delivery", () => {
+    const history = createMemoryHistory({
+      initialEntries: ["/delivery"],
+      initialIndex: 0,
+    });
+    const application = (
+      <Router history={history}>
+        <Provider store={store}>
+          <Application />
+        </Provider>
+      </Router>
+    );
+
+    render(application);
+    const helmet = Helmet.peek();
+    expect(helmet.title).toEqual("Delivery — Example store");
+  });
+  it("title должен быть Contacts", () => {
+    const history = createMemoryHistory({
+      initialEntries: ["/contacts"],
+      initialIndex: 0,
+    });
+    const application = (
+      <Router history={history}>
+        <Provider store={store}>
+          <Application />
+        </Provider>
+      </Router>
+    );
+
+    render(application);
+    const helmet = Helmet.peek();
+    expect(helmet.title).toEqual("Contacts — Example store");
   });
 });
