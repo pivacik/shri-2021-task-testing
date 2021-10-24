bea2c7e26085f6e87a7266a0e89b729e6ee0a93b
Dmitrii Pozdniakov <piv4cik@gmail.com>
2021-10-24 11:06:22 +0300

    test cart page without products in it

--- test/hermione/example.hermione.js
+++ test/hermione/example.hermione.js
@@ -1,45 +1,34 @@
 const { assert } = require("chai");
 
-describe("github", async function () {
-  it("Тест, который пройдет", async function () {
-    await this.browser.url("https://github.com/gemini-testing/hermione");
-    await this.browser.assertView("plain", "#readme", {
-      compositeImage: true,
-    });
-
-    const title = await this.browser.$("#readme h1").getText();
-    assert.equal(title, "Hermione");
-  });
-});
 describe("Статические страницы", async function () {
   it("Странца / должа иметь статическое содержимое", async function () {
     const browser = this.browser;
     await this.browser.url("/hw/store");
-    await browser.assertView("plain", ".Home", {
+    await browser.assertView("plain", ".Application", {
       compositeImage: true,
     });
   });
   it("Странца /contacts должа иметь статическое содержимое", async function () {
     const browser = this.browser;
     await this.browser.url("/hw/store/contacts");
-    await browser.assertView("plain", ".Contacts", {
+    await browser.assertView("plain", ".Application", {
       compositeImage: true,
     });
   });
   it("Странца /delivery должа иметь статическое содержимое", async function () {
     const browser = this.browser;
     await this.browser.url("/hw/store/delivery");
-    await browser.assertView("plain", ".Delivery", {
+    await browser.assertView("plain", ".Application", {
       compositeImage: true,
     });
   });
 });
 
-describe("Navbar", async function () {
-  it("Ссылки в navbar отображаюся", async function () {
+describe("Страница /cart", async function () {
+  it("Должна открывать с пустой корзиной", async function () {
     const browser = this.browser;
-    await this.browser.url("/hw/store");
-    await browser.assertView("plain", ".navbar", {
+    await this.browser.url("/hw/store/cart");
+    await browser.assertView("plain", ".Application", {
       compositeImage: true,
     });
   });
