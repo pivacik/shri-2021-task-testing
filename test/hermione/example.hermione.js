aaf1a87a79a4b4679e9c92c0415aa5ae70e98c38
Dmitrii Pozdniakov <piv4cik@gmail.com>
2021-10-24 13:10:14 +0300

    test if burger collapse

--- test/hermione/example.hermione.js
+++ test/hermione/example.hermione.js
@@ -33,3 +33,24 @@ describe("Страница /cart", async function () {
     });
   });
 });
+
+describe("Header при ширине меньше 576 ", async function () {
+  it("При выборе элемента из меню бургера, меню должно закрываться", async function () {
+    const browser = this.browser;
+    await browser.setWindowSize(575, 1000);
+    await browser.url("/hw/store/contacts");
+    await browser.assertView("plain", ".navbar", {
+      compositeImage: true,
+    });
+    await browser.$(".navbar-toggler").click();
+    await browser.pause(150);
+    await browser.assertView("plain_opened", ".navbar", {
+      compositeImage: true,
+    });
+    await browser.$(".nav-link.active").click();
+    await browser.pause(150);
+    await browser.assertView("plain-closed", ".navbar", {
+      compositeImage: true,
+    });
+  });
+});
