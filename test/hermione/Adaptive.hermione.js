13a8380f48dfa97ecd25fa374abb8e9620fa4cd2
Dmitrii Pozdniakov <piv4cik@gmail.com>
2021-11-08 09:52:22 +0300

    test adaptive catalog page

--- test/hermione/Adaptive.hermione.js
+++ test/hermione/Adaptive.hermione.js
@@ -1,62 +1,89 @@
-describe("Статические страницы", async function () {
-  it("Отображение адаптивной верстки Home 1100px", async function () {
+describe("Адаптивная верстка", async function () {
+  // it("Home 1100px", async function () {
+  //   const browser = this.browser;
+  //   await browser.setWindowSize(1100, 1200);
+  //   await this.browser.url("/hw/store");
+  //   await browser.assertView("plain", ".Home", {});
+  // });
+  // it("Home 900px", async function () {
+  //   const browser = this.browser;
+  //   await browser.setWindowSize(900, 1200);
+  //   await this.browser.url("/hw/store");
+  //   await browser.assertView("plain", ".Home", {});
+  // });
+  // it("Contacts 1100px", async function () {
+  //   const browser = this.browser;
+  //   await browser.setWindowSize(1100, 1200);
+  //   await this.browser.url("/hw/store/contacts");
+  //   await browser.assertView("plain", ".Contacts", {});
+  // });
+  // it("Contacts 900px", async function () {
+  //   const browser = this.browser;
+  //   await browser.setWindowSize(900, 1200);
+  //   await this.browser.url("/hw/store/contacts");
+  //   await browser.assertView("plain", ".Contacts", {});
+  // });
+  // it("Contacts 640px", async function () {
+  //   const browser = this.browser;
+  //   await browser.setWindowSize(640, 1200);
+  //   await this.browser.url("/hw/store/contacts");
+  //   await browser.assertView("plain", ".Contacts", {});
+  // });
+  // it("Contacts 400px", async function () {
+  //   const browser = this.browser;
+  //   await browser.setWindowSize(400, 1200);
+  //   await this.browser.url("/hw/store/contacts");
+  //   await browser.assertView("plain", ".Contacts", {});
+  // });
+  // it("Delivery 1100px", async function () {
+  //   const browser = this.browser;
+  //   await browser.setWindowSize(1100, 1200);
+  //   await this.browser.url("/hw/store/delivery");
+  //   await browser.assertView("plain", ".Delivery", {});
+  // });
+  // it("Delivery 900px", async function () {
+  //   const browser = this.browser;
+  //   await browser.setWindowSize(900, 1200);
+  //   await this.browser.url("/hw/store/delivery");
+  //   await browser.assertView("plain", ".Delivery", {});
+  // });
+  // it("Delivery 640px", async function () {
+  //   const browser = this.browser;
+  //   await browser.setWindowSize(640, 1200);
+  //   await this.browser.url("/hw/store/delivery");
+  //   await browser.assertView("plain", ".Delivery", {});
+  // });
+  // it("Delivery 400px", async function () {
+  //   const browser = this.browser;
+  //   await browser.setWindowSize(400, 1200);
+  //   await this.browser.url("/hw/store/delivery");
+  //   await browser.assertView("plain", ".Delivery", {});
+  // });
+  it("Catalog 700px", async function () {
     const browser = this.browser;
-    await browser.setWindowSize(1100, 1200);
-    await this.browser.url("/hw/store");
-    await browser.assertView("plain", ".Home", {});
+    await browser.setWindowSize(700, 900);
+    await browser.url("/hw/store/catalog");
+
+    const card = await browser.$(".ProductItem");
+    await browser.waitUntil(() => card.isDisplayedInViewport(), 3000);
+    await browser.assertView("plain", ".Catalog", {
+      allowViewportOverflow: true,
+      captureElementFromTop: false,
+      ignoreElements: [".ProductItem-Price", ".ProductItem-Name"],
+    });
   });
-  it("Отображение адаптивной верстки Home 900px", async function () {
+
+  it("Catalog 400px", async function () {
     const browser = this.browser;
-    await browser.setWindowSize(900, 1200);
-    await this.browser.url("/hw/store");
-    await browser.assertView("plain", ".Home", {});
-  });
-  it("Отображение адаптивной верстки Contacts 1100px", async function () {
-    const browser = this.browser;
-    await browser.setWindowSize(1100, 1200);
-    await this.browser.url("/hw/store/contacts");
-    await browser.assertView("plain", ".Contacts", {});
-  });
-  it("Отображение адаптивной верстки Contacts 900px", async function () {
-    const browser = this.browser;
-    await browser.setWindowSize(900, 1200);
-    await this.browser.url("/hw/store/contacts");
-    await browser.assertView("plain", ".Contacts", {});
-  });
-  it("Отображение адаптивной верстки Contacts 640px", async function () {
-    const browser = this.browser;
-    await browser.setWindowSize(640, 1200);
-    await this.browser.url("/hw/store/contacts");
-    await browser.assertView("plain", ".Contacts", {});
-  });
-  it("Отображение адаптивной верстки Contacts 400px", async function () {
-    const browser = this.browser;
-    await browser.setWindowSize(400, 1200);
-    await this.browser.url("/hw/store/contacts");
-    await browser.assertView("plain", ".Contacts", {});
-  });
-  it("Отображение адаптивной верстки Delivery 1100px", async function () {
-    const browser = this.browser;
-    await browser.setWindowSize(1100, 1200);
-    await this.browser.url("/hw/store/delivery");
-    await browser.assertView("plain", ".Delivery", {});
-  });
-  it("Отображение адаптивной верстки Delivery 900px", async function () {
-    const browser = this.browser;
-    await browser.setWindowSize(900, 1200);
-    await this.browser.url("/hw/store/delivery");
-    await browser.assertView("plain", ".Delivery", {});
-  });
-  it("Отображение адаптивной верстки Delivery 640px", async function () {
-    const browser = this.browser;
-    await browser.setWindowSize(640, 1200);
-    await this.browser.url("/hw/store/delivery");
-    await browser.assertView("plain", ".Delivery", {});
-  });
-  it("Отображение адаптивной верстки Delivery 400px", async function () {
-    const browser = this.browser;
-    await browser.setWindowSize(400, 1200);
-    await this.browser.url("/hw/store/delivery");
-    await browser.assertView("plain", ".Delivery", {});
+    await browser.setWindowSize(400, 900);
+    await browser.url("/hw/store/catalog");
+
+    const card = await browser.$(".ProductItem");
+    await browser.waitUntil(() => card.isDisplayedInViewport(), 3000);
+    await browser.assertView("plain", ".Catalog", {
+      allowViewportOverflow: true,
+      captureElementFromTop: false,
+      ignoreElements: [".ProductItem-Price", ".ProductItem-Name"],
+    });
   });
 });
